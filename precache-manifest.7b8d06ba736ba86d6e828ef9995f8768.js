7583c22a744272d7ccd23f842e47a97c49a6f0a1
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-06-26 10:36:50 +0530

    Updates

---
+++ precache-manifest.7b8d06ba736ba86d6e828ef9995f8768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c526dc365aa07d533f483927aabc5c0",
+    "url": "/user-auth-portal/index.html"
+  },
+  {
+    "revision": "6c277e1529220c62ca2e",
+    "url": "/user-auth-portal/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "260d17ecafb2addb0a38",
+    "url": "/user-auth-portal/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6c277e1529220c62ca2e",
+    "url": "/user-auth-portal/static/js/2.74995013.chunk.js"
+  },
+  {
+    "revision": "260d17ecafb2addb0a38",
+    "url": "/user-auth-portal/static/js/main.8b608e88.chunk.js"
+  },
+  {
+    "revision": "427017091f6c02eb33e2",
+    "url": "/user-auth-portal/static/js/runtime~main.7028951b.js"
+  }
+]);
